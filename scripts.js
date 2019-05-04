5af861f575b659053caff6cd66b5a6549369eb33
lendilai <ngethenan768@gmail.com>
2019-05-04 21:23:20 +0300

    Integrated mailchimp API key

--- scripts.js
+++ scripts.js
@@ -10,3 +10,14 @@ $(".clickable3").click(function(){
   $(".management-hidden").slideToggle();
   $(".management-showing").slideToggle();
 });
+var key = '1c2089a31a00d28af6cb0e553ed84697-us20';
+$("button").click(function(event) {
+  event.preventDefault();
+  var user = document.getElementById('username').value;
+  alert("Dear " + user + ", we have received your message. Thank you for reaching out to us.");
+});
+$("button").on('click', function(){
+  $('form').each(function(){
+    this.reset();
+  });
+});
